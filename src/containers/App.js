c2c7760f798337d5d6bc289e88a567eece6eba36
Kirill Ponomarenko <kir-rik-90@yandex.ru>
2017-07-02 15:52:51 +0400

    redux tutorial complete

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
+import User from '../components/User';
+import Page from '../components/Page';
+import * as pageActions from '../actions/PageActions';
+
 import logo from '../logo.svg';
 import '../App.css';
 
 class App extends Component {
     render() {
-        const { name, surname, age } = this.props.user
+        const { user, page } = this.props;
+        const { getPhotos } = this.props.pageActions;
         return (
-                <div className="App">
-                    <div className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <h2>Welcome to React</h2>
-                    </div>
-                    <div className="App-intro">
-                        <p>Привет из App, {name} {surname}!</p>
-                        <p>Тебе уже {age} ?</p>
-                    </div>
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
                 </div>
+                <div>
+                    <User name={user.name} />
+                    <Page photos={page.photos} year={page.year} getPhotos={getPhotos} fetching={page.fetching}/>
+                </div>
+            </div>
         )
     }
 }
 
 function mapStateToProps (state) {
     return {
-        user: state
+        user: state.user, // (1)
+        page: state.page // (2)
     }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps(dispatch) {
+    return {
+        pageActions: bindActionCreators(pageActions, dispatch)
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
